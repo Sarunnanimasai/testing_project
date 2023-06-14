1a28af9bd0c230dc340bd3d87b164e9a7f487831
Sama Sarun Reddy <sarunnanimasaischool@gmail.com>
2023-06-14 14:38:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import HomePage from "./components/homepage";
+import AllRoutes from "./AllRoutes";
+import Navbar from "./components/navbar";
+
+function App() {
+  return <AllRoutes />;
+}
+
+export default App;
